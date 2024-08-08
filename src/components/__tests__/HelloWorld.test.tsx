7ec59b9c771f1a01e2707aca10a1d3281336877c
Andrew, Muhn <Andrew.Muhn@cvent.com>
2024-08-08 08:24:46 -0700

    repo setup dependency install

---
+++ src/components/__tests__/HelloWorld.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import HelloWorld from '../HelloWorld';
+
+test('renders Hello, world!', () => {
+  render(<HelloWorld />);
+  const element = screen.getByText(/Hello, world!/i);
+  expect(element).toBeInTheDocument();
+});
