9db6a6e5d8cc9f0a75b6115a21ea2db680840a75
Andrew, Muhn <Andrew.Muhn@cvent.com>
2024-08-08 13:47:26 -0700

    removed helloWorld test

--- src/components/__tests__/HelloWorld.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import HelloWorld from '../HelloWorld';
-
-test('renders Hello, world!', () => {
-  render(<HelloWorld />);
-  const element = screen.getByText(/Hello, world!/i);
-  expect(element).toBeInTheDocument();
-});
