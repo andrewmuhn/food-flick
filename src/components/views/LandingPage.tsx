b6d17dbb2070eab54f94d5f436c8dca18c75b891
jared.harvick <jared.harvick@cvent.com>
2024-08-13 14:28:46 -0500

    Added modals to landing page

--- src/components/views/LandingPage.tsx
+++ src/components/views/LandingPage.tsx
@@ -1,48 +1,137 @@
 import React, { useState } from "react";
+import '../../index.css';
 
 const LandingPage: React.FC = () => {
     const [selectedDinnerParty, setSelectedDinnerParty] = useState<string>("");
     const [selectedHostedParty, setSelectedHostedParty] = useState<string>("");
+    const [isPartyModalOpen, setIsPartyModalOpen] = useState<boolean>(false);
+    const [isRestaurantModalOpen, setIsRestaurantModalOpen] = useState<boolean>(false);
 
     const handleCreateParty = () => {
-        // Logic for creating a party
-        console.log("Create party button clicked");
+        setIsPartyModalOpen(true);
+    };
+
+    const handlePartySubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        // Logic to create a dinner party in the database
+        setIsPartyModalOpen(false);
+        setIsRestaurantModalOpen(true);
+    };
+
+    const handleRestaurantSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        // Logic to create the list of restaurants for the party in the database
+        setIsRestaurantModalOpen(false);
     };
 
     return (
-        <div className="p-6 max-w-md mx-auto bg-white rounded-xl shadow-md space-y-4">
-            <div>
-                <select
-                    id="dinnerParties"
-                    value={selectedDinnerParty}
-                    onChange={(e) => setSelectedDinnerParty(e.target.value)}
-                    className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+        <div>
+            <div className={`p-6 max-w-md mx-auto bg-white rounded-xl shadow-md space-y-4 ${isPartyModalOpen || isRestaurantModalOpen ? 'blur' : ''}`}>
+                <div>
+                    <label htmlFor="dinnerParties" className="block text-sm font-medium text-gray-700">Dinner Parties</label>
+                    <select
+                        id="dinnerParties"
+                        value={selectedDinnerParty}
+                        onChange={(e) => setSelectedDinnerParty(e.target.value)}
+                        className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                    >
+                        <option value="">Select a dinner party</option>
+                        {/* Add options dynamically or statically */}
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="hostedParties" className="block text-sm font-medium text-gray-700">Your hosted parties</label>
+                    <select
+                        id="hostedParties"
+                        value={selectedHostedParty}
+                        onChange={(e) => setSelectedHostedParty(e.target.value)}
+                        className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                    >
+                        <option value="">Select a hosted party</option>
+                        {/* Add options dynamically or statically */}
+                    </select>
+                </div>
+                <button
+                    onClick={handleCreateParty}
+                    className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green hover:bg-green-dark focus:bg-green-dark focus:outline-none"
                 >
-                    <option value="">Select a dinner party</option>
-                    {/* Add options dynamically or statically */}
-                    <option value="party1">Party 1</option>
-                    <option value="party2">Party 2</option>
-                </select>
+                    Create party
+                </button>
             </div>
-            <div>
-                <select
-                    id="hostedParties"
-                    value={selectedHostedParty}
-                    onChange={(e) => setSelectedHostedParty(e.target.value)}
-                    className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-                >
-                    <option value="">Select a hosted party</option>
-                    {/* Add options dynamically or statically */}
-                    <option value="hosted1">Hosted Party 1</option>
-                    <option value="hosted2">Hosted Party 2</option>
-                </select>
-            </div>
-            <button
-                onClick={handleCreateParty}
-                className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-            >
-                Create party
-            </button>
+
+            {isPartyModalOpen && (
+                <div className="fixed inset-0 flex items-center justify-center z-50">
+                    <div className="bg-white p-6 rounded-lg shadow-lg w-2/4 h-50">
+                        <h2 className="text-xl font-bold mb-4">Party Details</h2>
+                        <form onSubmit={handlePartySubmit}>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Name</label>
+                                <input type="text" className="mt-1 block w-full p-2 border border-gray-300 rounded-md" required />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Location</label>
+                                <input type="text" className="mt-1 block w-full p-2 border border-gray-300 rounded-md" required />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Date</label>
+                                <input type="date" className="mt-1 block w-full p-2 border border-gray-300 rounded-md" required />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Time</label>
+                                <input type="time" className="mt-1 block w-full p-2 border border-gray-300 rounded-md" required />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Voting Strategy</label>
+                                <select className="mt-1 block w-full p-2 border border-gray-300 rounded-md" required>
+                                    <option value="">Select strategy</option>
+                                    <option value="strategy1">Strategy 1</option>
+                                    <option value="strategy2">Strategy 2</option>
+                                </select>
+                            </div>
+                            <button
+                              type="submit"
+                              className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green hover:bg-green-dark focus:bg-green-dark focus:outline-none"
+                            >
+                              Submit
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            )}
+
+            {isRestaurantModalOpen && (
+                <div className="fixed inset-0 flex items-center justify-center z-55">
+                    <div className="bg-white p-6 rounded-lg shadow-lg w-2/4 h-50">
+                        <h2 className="text-xl font-bold mb-4">Filter Restaurants</h2>
+                        <form onSubmit={handleRestaurantSubmit}>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Price Range</label>
+                                <div className="flex justify-between text-xs px-2">
+                                    <span>$</span>
+                                    <span className="ml-4">$$</span>
+                                    <span className="ml-2">$$$</span>
+                                    <span className="ml-2">$$$$</span>
+                                    <span>$$$$$</span>
+                                </div>
+                                <input type="range" min="1" max="5" className="mt-1 block w-full slider" />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Radius(in miles)</label>
+                                <input type="number" className="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder="MAX 25" max="25" required />
+                            </div>
+                            <div className="mb-4">
+                                <input type="checkbox" name="vegetarian" className="mr-2" defaultChecked={false} /> Vegetarian
+                            </div>
+                            <div className="mb-4">
+                                <input type="checkbox" name="vegan" className="mr-2" defaultChecked={false} /> Vegan
+                            </div>
+                            <button type="submit" className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green hover:bg-green-dark focus:bg-green-dark focus:outline-none">
+                                Submit
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,25 @@ export default {
   theme: {
     extend: {
       colors: {
-        green: "#4B5A35",
+        green: {
+          DEFAULT: "#4B5A35",
+          dark: "#3E4A2B", // Slightly darker shade for hover/focus
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.slider-thumb': {
+          'background-color': '#4B5A35', // Custom green color
+          'border': '2px solid #4B5A35', // Custom green color
+          'height': '16px',
+          'width': '16px',
+          'border-radius': '50%',
+          'cursor': 'pointer',
+        },
+      }, ['responsive', 'hover']);
+    },
+  ],
 };
