02d84bdd47eee75191db198abbce48c6282b69c5
lesleytomosada <tomosadalesley@gmail.com>
2024-08-16 07:25:54 -0700

    basic navbar template

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
-import LandingPage from './components/views/LandingPage';
-import { Authenticator } from '@aws-amplify/ui-react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./components/views/LandingPage";
+import { Authenticator } from "@aws-amplify/ui-react";
+import NavBar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
       <Authenticator>
-      <Router>
-        <Routes>
-          <Route path="/" Component={LandingPage}/>
-        </Routes>
-      </Router>
+        <NavBar />
+        <Router>
+          <Routes>
+            <Route path="/" Component={LandingPage} />
+          </Routes>
+        </Router>
       </Authenticator>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,21 +9,25 @@ export default {
           DEFAULT: "#4B5A35",
           dark: "#3E4A2B", // Slightly darker shade for hover/focus
         },
+        dun: "#CDC2B6",
       },
     },
   },
   plugins: [
     function ({ addUtilities }) {
-      addUtilities({
-        '.slider-thumb': {
-          'background-color': '#4B5A35', // Custom green color
-          'border': '2px solid #4B5A35', // Custom green color
-          'height': '16px',
-          'width': '16px',
-          'border-radius': '50%',
-          'cursor': 'pointer',
+      addUtilities(
+        {
+          ".slider-thumb": {
+            "background-color": "#4B5A35", // Custom green color
+            border: "2px solid #4B5A35", // Custom green color
+            height: "16px",
+            width: "16px",
+            "border-radius": "50%",
+            cursor: "pointer",
+          },
         },
-      }, ['responsive', 'hover']);
+        ["responsive", "hover"]
+      );
     },
   ],
 };
