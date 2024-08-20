5ab947715e9d7c48d2fed5a7e46d49b2a825bde7
Aidan Liddiard <Aidan.Liddiard@cvent.com>
2024-08-19 17:20:14 -0700

    stying changes

--- src/components/views/VotingPage.tsx
+++ src/components/views/VotingPage.tsx
@@ -48,7 +48,7 @@ const VotingPage: React.FC = () => {
             key={restaurant.restaurant_id}
             restaurant={restaurant}
             cardIndex={index}
-            resturantArrayLength={restaurants.length}
+            restaurantArrayLength={restaurants.length}
           />
         ))
       ) : (
