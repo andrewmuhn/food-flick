08c0d63322e6c7938c57e3600b6ae793d40df647
Aidan Liddiard <Aidan.Liddiard@cvent.com>
2024-08-20 10:25:43 -0700

    started dinnerPartyContext

---
+++ src/components/Context/DinnerPartyContext.tsx
@@ -0,0 +1,100 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { useParams } from "react-router-dom";
+import { getDinnerPartyById } from "../../services/DinnerPartyService";
+import { Restaurant } from "../../models/Restaurant";
+import { DinnerParty } from "../../models/DinnerParty";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+
+interface DinnerPartyContextProps {
+  dinnerParty: DinnerParty;
+  restaurants: Restaurant[];
+  loading: boolean;
+  error: string | null;
+}
+
+const DinnerPartyContext = createContext<DinnerPartyContextProps | undefined>(
+  undefined
+);
+
+export const DinnerPartyProvider = ({ children }: { children: ReactNode }) => {
+  const { dinnerPartyId } = useParams<{ dinnerPartyId: string }>();
+  const [dinnerParty, setDinnerParty] = useState<DinnerParty>();
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const dinnerPartyId = Number(window.location.pathname.split("/")[2]);
+
+    console.log(dinnerPartyId);
+    if (!dinnerPartyId) return;
+
+    const fetchDinnerParty = async (id: number) => {
+      try {
+        const response = await getDinnerPartyById(id);
+        setDinnerParty(response);
+        filterRestaurantsToRender(response);
+        setLoading(false);
+      } catch (err) {
+        setError("Failed to fetch dinner party info");
+        setLoading(false);
+      }
+    };
+
+    fetchDinnerParty(dinnerPartyId);
+  }, [dinnerPartyId]);
+
+  const { user } = useAuthenticator((context) => [context.user]);
+
+  const filterRestaurantsToRender = (dinnerParty: DinnerParty) => {
+    const restaurantsToRender: Restaurant[] = [];
+    dinnerParty.restaurants.forEach((restaurant) => {
+      restaurant.votes.forEach((vote) => {
+        if (vote.created_by === user.userId) {
+          return;
+        }
+        restaurantsToRender.push(restaurant);
+        setRestaurants(restaurantsToRender);
+      });
+    });
+    setRestaurants(dinnerParty.restaurants);
+  };
+
+  const removeRestaurantToRender = (restaurantId: number) => {
+    const restaurantsRendering: Restaurant[] = restaurants;
+    restaurantsRendering.forEach((restaurant, index) => {
+      if (restaurant.restaurant_id === restaurantId) {
+        restaurantsRendering.splice(index, 1);
+      }
+      setRestaurants(restaurantsRendering);
+    });
+  };
+
+  return (
+    <DinnerPartyContext.Provider
+      value={{
+        dinnerParty,
+        restaurants,
+        loading,
+        error,
+        removeRestaurantToRender,
+      }}
+    >
+      {children}
+    </DinnerPartyContext.Provider>
+  );
+};
+
+export const useDinnerPartyContext = () => {
+  const context = useContext(DinnerPartyContext);
+  if (context === undefined) {
+    throw new Error("useDinnerParty must be used within a DinnerPartyProvider");
+  }
+  return context;
+};
--- src/components/views/VotingPage.tsx
+++ src/components/views/VotingPage.tsx
@@ -3,31 +3,34 @@ import CarouselCard from "../CarouselCard";
 import { Restaurant } from "../../models/Restaurant";
 import { getDinnerPartyById } from "../../services/DinnerPartyService";
 import LoadingState from "../LoadingState";
+import { useDinnerPartyContext } from "../Context/DinnerPartyContext";
 
 const VotingPage: React.FC = () => {
-  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+  // const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const pathParameter = Number(window.location.pathname.split("/")[2]);
+  // useEffect(() => {
+  //   const pathParameter = Number(window.location.pathname.split("/")[2]);
 
-    const fetchDinnerParty = async (dinnerPartyId: number) => {
-      // Now you can use the 'path' variable to access the path parameter
-      try {
-        const dinnerParty = await getDinnerPartyById(dinnerPartyId);
-        console.log(dinnerParty.restaurants);
-        setRestaurants(dinnerParty.restaurants);
-        
-        setLoading(false);
-      } catch (err) {
-        setError("Failed to fetch dinner party info");
-        setLoading(false);
-      }
-    };
+  //   const fetchDinnerParty = async (dinnerPartyId: number) => {
+  //     // Now you can use the 'path' variable to access the path parameter
+  //     try {
+  //       const dinnerParty = await getDinnerPartyById(dinnerPartyId);
+  //       console.log(dinnerParty.restaurants);
+  //       setRestaurants(dinnerParty.restaurants);
 
-    fetchDinnerParty(pathParameter);
-  }, []);
+  //       setLoading(false);
+  //     } catch (err) {
+  //       setError("Failed to fetch dinner party info");
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   fetchDinnerParty(pathParameter);
+  // }, []);
+
+  const { restaurants, loading, error } = useDinnerPartyContext();
 
   if (loading) {
     return (
