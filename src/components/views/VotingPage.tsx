18f6ac761d2be8be2244d9b86162a308dc56a5ef
lesleytomosada <tomosadalesley@gmail.com>
2024-08-21 08:11:57 -0700

    changes to get lock votes button to work

--- src/components/views/VotingPage.tsx
+++ src/components/views/VotingPage.tsx
@@ -6,10 +6,9 @@ import { useAuthenticator } from "@aws-amplify/ui-react";
 import LockVotesButton from "../LockVotesButton";
 
 const VotingPage: React.FC = () => {
-  const { restaurants, dinnerParty, loading, error } =
-    useDinnerPartyContext();
+  const { restaurants, dinnerParty, loading, error } = useDinnerPartyContext();
 
-    const { user } = useAuthenticator();
+  const { user } = useAuthenticator();
 
   if (loading) {
     return (
@@ -26,6 +25,8 @@ const VotingPage: React.FC = () => {
       </div>
     );
   }
+
+  console.log(dinnerParty);
 
   if (!dinnerParty) return null;
 
--- src/services/DinnerPartyService.ts
+++ src/services/DinnerPartyService.ts
@@ -2,40 +2,62 @@ import { DinnerParty } from "../models/DinnerParty";
 import dinnerPartyApiInstance from "../utils/DinnerPartyApiInstance";
 
 export const postNewDinnerParty = async (dinnerParty: DinnerParty) => {
-    try {
-        const response = await dinnerPartyApiInstance.post<DinnerParty>('/dinner-party', dinnerParty);
-        return response.data;
-    } catch (error) {
-        console.error('Failed to post new dinner party:\n', error);
-        throw error;
-    }
-}
+  try {
+    const response = await dinnerPartyApiInstance.post<DinnerParty>(
+      "/dinner-party",
+      dinnerParty
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Failed to post new dinner party:\n", error);
+    throw error;
+  }
+};
 
 export const getAllDinnerParties = async (): Promise<DinnerParty[]> => {
-    try {
-        const response = await dinnerPartyApiInstance.get<DinnerParty[]>('/dinner-party');
-        return response.data;
-    } catch (error) {
-        console.error('Failed to fetch dinner parties:\n', error);
-        throw error
-    }
-}
+  try {
+    const response = await dinnerPartyApiInstance.get<DinnerParty[]>(
+      "/dinner-party"
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch dinner parties:\n", error);
+    throw error;
+  }
+};
 
-export const getDinnerPartyById = async (dinnerPartyId:number): Promise<DinnerParty> => {
-    try {
-        const response = await dinnerPartyApiInstance.get<DinnerParty>(`/dinner-party/${dinnerPartyId}`);
-        return response.data;
-    } catch (error) {
-        console.error('Failed to fetch dinner parties:\n', error);
-        throw error
-    }
-}
+export const getDinnerPartyById = async (
+  dinnerPartyId: number
+): Promise<DinnerParty> => {
+  try {
+    const response = await dinnerPartyApiInstance.get<DinnerParty>(
+      `/dinner-party/${dinnerPartyId}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch dinner parties:\n", error);
+    throw error;
+  }
+};
 
-export const updateDinnerPartyById = async (dinnerPartyId:number): Promise<void> => {
-    try {
-        await dinnerPartyApiInstance.put<DinnerParty>(`/dinner-party/${dinnerPartyId}`);
-    } catch (error) {
-        console.error('Failed to update dinner party:\n', error);
-        
-    }
-}
+export const updateDinnerPartyById = async (
+  dinnerPartyId: number
+): Promise<void> => {
+  try {
+    await dinnerPartyApiInstance.put<DinnerParty>(
+      `/dinner-party/${dinnerPartyId}`
+    );
+  } catch (error) {
+    console.error("Failed to update dinner party:\n", error);
+  }
+};
+
+export const lockVotes = async (dinnerPartyId: number): Promise<void> => {
+  try {
+    await dinnerPartyApiInstance.put<DinnerParty>(
+      `/dinner-party/${dinnerPartyId}/lock`
+    );
+  } catch (error) {
+    console.error("Failed to update dinner party:\n", error);
+  }
+};
